d7ffbe110ad10cc877ecfb5a4eadd71411ac4df4
rokussadee <rokus.sadee@student.ehb.be>
2023-10-22 21:27:38 +0200

    feat(sockets): socket.io setup
    vite.config.ts: webSocketServer plugin & basic emitter
    svelte.config.js: remove deprecated 'config.kit.vite' prop
    +page.svelte: basic socket client listener

--- webrtc-sveltekit/src/routes/+page.svelte
+++ webrtc-sveltekit/src/routes/+page.svelte
@@ -1,6 +1,13 @@
-<script>
-
+<script lang="ts">
   import Video from '../components/Video.svelte'
+  import { io } from 'socket.io-client';
+
+  const socket = io()
+
+  socket.on('eventFromServer', (message)=> {
+    console.log(message)
+  })
+
 </script>
 
 <div>
--- webrtc-sveltekit/vite.config.ts
+++ webrtc-sveltekit/vite.config.ts
@@ -1,8 +1,27 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import type { ViteDevServer } from 'vite';
 import { defineConfig } from 'vitest/config';
+import {Server} from 'socket.io';
+
+export const webSocketServer = {
+  name:'webSocketServer',
+  configureServer(server: ViteDevServer) {
+    if(!server.httpServer) return
+    const io = new Server(server.httpServer)
+
+    io.on('connect',(socket) => {
+      socket.emit('eventFromServer', 'Connected')
+    })
+  }
+}
+
+const vitePlugins = [webSocketServer]
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+    ...vitePlugins,
+    sveltekit()
+  ],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
